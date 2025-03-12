c996070350a24f3af6c768e08c090a0e39a89ee1
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 07:07:26 +0700

    first commit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300038
+{
+    internal class Class1
+    {
+    }
+}
